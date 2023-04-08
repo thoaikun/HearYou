346bf079b2e00d0004d1aab9c933eac4768bc769
Nguyen Duc An <an.nguyenduc1406@hcmut.edu.vn>
2023-04-08 17:35:23 +0700

    update features/music-player

--- mobile/App.js
+++ mobile/App.js
@@ -1,9 +1,11 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import LoginScreen from './src/screens/LoginScreen/index'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './src/screens/LoginScreen/index';
+import Title from './src/components/Title/Title';
+import MusicPlayer from './src/screens/MusicPlayer';
 export default function App() {
-    return (
-        <SafeAreaView>
-            <LoginScreen />
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView>
+      <MusicPlayer />
+    </SafeAreaView>
+  );
 }
--- mobile/src/screens/LoginScreen/index.jsx
+++ mobile/src/screens/LoginScreen/index.jsx
@@ -1,20 +1,20 @@
-import * as React from 'react'
-import { Image, Text, View } from 'react-native'
-import Button from '../../components/button/Button'
-import styles from './styles'
+import * as React from 'react';
+import { Image, Text, View } from 'react-native';
+import Button from '../../components/button/Button';
+import styles from './styles';
 
 const LoginScreen = () => {
-    return (
-        <View style={styles.container}>
-            <View style={styles.image}>
-                <Image source={require('../../../assets/infographic.png')} />
-            </View>
-            <View style={styles.logo}>
-                <Image source={require('../../../assets/logo.png')} />
-            </View>
-            <Button content='Login'></Button>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image source={require('../../../assets/infographic.png')} />
+      </View>
+      <View style={styles.logo}>
+        <Image source={require('../../../assets/logo.png')} />
+      </View>
+      <Button content="Login"></Button>
+    </View>
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
