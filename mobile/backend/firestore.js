ce9276ef175747d2dcf826fa21e430eb4de9d954
Hùng Nguyễn Hoàng Bảo <hung.nhb@gmail.com>
2023-04-08 15:12:37 +0700

    firestore helper functions

--- mobile/backend/firestore.js
+++ mobile/backend/firestore.js
@@ -1,1 +1,130 @@
 // TODO: firestore helper functions
+// @ts-check
+
+import { firestore } from '../config/firebase'
+import { collection, doc, getDoc, getDocs, limit, orderBy, query, setDoc, where } from "firebase/firestore";
+import { v4 as uuidv4 } from 'uuid';
+
+// Users
+
+export const getUserData = async (uid) => {
+    const docSnap = await getDoc(doc(firestore, "users", uid))
+    if (docSnap.exists())
+        return docSnap.data()
+    else
+        return null
+}
+
+export const addNewUser = async (payload) => {
+    await setDoc(doc(firestore, "users", payload.uid), {
+        name: payload.name,
+        favoritePodcast: [],
+        role: payload.role
+    })
+}
+
+// Podcast
+
+export const getPodcastData = async (pid) => {
+    const docSnap = await getDoc(doc(firestore, "podcasts", pid))
+    if (docSnap.exists())
+        return docSnap.data()
+    else
+        return null
+}
+
+// Episodes
+
+export const getEpisodeData = async (eid) => {
+    const docSnap = await getDoc(doc(firestore, "episodes", eid))
+    if (docSnap.exists())
+        return docSnap.data()
+    else
+        return null
+}
+
+export const getNewestEpisode = async () => {
+    const q = query(collection(firestore, "episodes"), orderBy("createdAt", "desc"), limit(10))
+    const querySnapshot = await getDocs(q)
+    const listEpisodes = []
+    querySnapshot.forEach((doc) => {
+        listEpisodes.push(doc.data())
+    })
+    return listEpisodes
+}
+
+export const getTrendingEpisode = async () => {
+    const q = query(collection(firestore, "episodes"), orderBy("view"), limit(10))
+    const querySnapshot = await getDocs(q)
+    const listEpisodes = []
+    querySnapshot.forEach((doc) => {
+        listEpisodes.push(doc.data())
+    })
+    return listEpisodes
+}
+
+export const getEpisodeByTopic = async (topic) => {
+    const q = query(collection(firestore, "episodes"), where("topic", "==", topic), limit(10))
+    const querySnapshot = await getDocs(q)
+    const listEpisodes = []
+    querySnapshot.forEach((doc) => {
+        listEpisodes.push(doc.data())
+    })
+    return listEpisodes
+}
+
+export const addNewEpisode = async (payload) => {
+    const episodeID = uuidv4()
+    await setDoc(doc(firestore, "episodes", episodeID), {
+        episodeID: episodeID,
+        title: payload.title,
+        description: payload.description,
+        view: 0,
+        topic: payload.topic,
+        url: payload.url,
+        authorID: payload.authorID,
+        authorName: payload.authorName,
+        createdAt: new Date(),
+        questionsID: [],
+        advertiseID: null
+    })
+}
+
+// Advertises
+
+export const getAdvertise = async (aid) => {
+    const docSnap = await getDoc(doc(firestore, "advertises", aid))
+    if (docSnap.exists())
+        return docSnap.data()
+    else
+        return null
+}
+
+export const addNewAdvertise = async (payload) => {
+    const adsID = uuidv4()
+    await setDoc(doc(firestore, "advertises", adsID), {
+        advertiseID: adsID,
+        title: payload.title,
+        company: payload.company,
+        url: payload.url
+    })
+}
+
+// Questions
+
+export const getQuestion = async (qid) => {
+    const docSnap = await getDoc(doc(firestore, "questions", qid))
+    if (docSnap.exists())
+        return docSnap.data()
+    else
+        return null
+}
+
+export const addNewQuestion = async (payload) => {
+    const quesID = uuidv4()
+    await setDoc(doc(firestore, "questions", quesID), {
+        questionID: quesID,
+        description: payload.description,
+        episodeID: payload.episodeID
+    })
+}
