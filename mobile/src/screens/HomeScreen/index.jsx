76ba15e38518ccada29396e6a48f1b6a1b1b14b2
Hùng Nguyễn Hoàng Bảo <hung.nhb@gmail.com>
2023-04-08 21:21:57 +0700

    add Profile and Your list Screens

--- mobile/src/screens/HomeScreen/index.jsx
+++ mobile/src/screens/HomeScreen/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Playing } from "../../components/playing/Playing";
+import Playing from "../../components/playing/Playing";
 import { View } from "react-native";
 import styles from "./styles";
 
-export const HomeScreen = () => {
+export default HomeScreen = () => {
     return (
         <View style={styles.container}>
             <Playing />
